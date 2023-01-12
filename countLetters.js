2f1a1eaed158c597d1bf935527b1af1be0d2e516
Juan Escobar <jcarriola83@gmail.com>
2023-01-12 02:36:45 +0000

    countLetters

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+        console.log(`👍👍👍  Assertion Passed: ${actual} === ${expected}`)
+    } else {
+        console.log(`👎👎👎  Assertion Failed: ${actual} !== ${expected}`)
+    }
+};
+
+function countLetters (sentence) {
+    let list = {}
+    
+    for (const letter of sentence) {
+        
+        if(list[letter]) {
+            list[letter] += 1
+        } else {
+            list[letter] = 1
+        }
+    }
+    
+    return list
+}
+
+

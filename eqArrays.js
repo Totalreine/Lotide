1319f4cee0ee3833c1f7414ea484b70189fd9660
Juan Escobar <jcarriola83@gmail.com>
2023-01-17 05:49:30 +0000

    test with mocha and chai

--- eqArrays.js
+++ eqArrays.js
@@ -2,9 +2,7 @@ const assertEqual = require("./assertEqual")
 
 function eqArrays (array1, array2) {
     let r = ""
-    if(array1.length === 0 && array2.length === 0){
-        return true
-    }
+
     for (let i = 0; i < array1.length; i++) {
         if(array1[i] !== array2[i]) {
              return false

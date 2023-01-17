9c41becb4e884edc0a153f484f3490934590329d
Juan Escobar <jcarriola83@gmail.com>
2023-01-17 05:38:16 +0000

    refactoring with mocha and chai

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,6 +1,16 @@
 
-const assertEqual = require('../assertEqual');
+/*const assertEqual = require('../assertEqual');
 
-assertEqual(6,6)
+assertEqual(6,6) */
 
-  
+const assert = require('chai').assert;
+const assertEqual = require('../assertEqual')
+
+describe("#assertEqual", () => {
+  it("returns 6 for 6", () => {
+    assert.strictEqual(assertEqual(6, 6));
+  });
+  it("returns ['5','6'] for ['5','6']", () => {
+    assert.strictEqual(assertEqual(['5','6'], ['5','6'])); 
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,17 @@
-const assertEqual = require("../assertEqual")
+/*const assertEqual = require("../assertEqual")
 const tail = require('../tail');
 
-assertEqual(tail([4,7,2]),[7,2])
+assertEqual(tail([4,7,2]),[7,2])*/
 
   
+const assert = require('chai').assert;
+const tail = require('../tail');
+
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2,3]);
+  });
+  it("returns ['7','9','5'] for ['5','7','9','5']", () => {
+    assert.deepEqual(tail(['5','7','9','5']), ["7","9","5"]); 
+  });
+});
